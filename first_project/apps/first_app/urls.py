5fc5c91dff951cf9308209d55114b75910aec28c
JoshuaHolloway <jhollow6@asu.edu>
2019-06-15 14:43:25 -0500

    push directly prior to submission

--- first_project/apps/first_app/urls.py
+++ first_project/apps/first_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^create$', views.create),
+    url(r'^new$', views.new),
+    url(r'^(?P<my_val>\d+)$', views.show),
+    url(r'^(?P<my_val>\d+)/edit$', views.edit),
+    url(r'^(?P<my_val>\d+)/destroy$', views.destroy)
 ]
--- first_project/apps/first_app/views.py
+++ first_project/apps/first_app/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def show(request, my_val):
+    return HttpResponse("placeholder to display blog number: " + str(my_val))
+
+def edit(request, my_val):
+    return HttpResponse("placeholder to edit blog: " + str(my_val))
+
+def create(request):
+    return redirect("/")
+
+def destroy(request, my_val):
+    return redirect("/")
