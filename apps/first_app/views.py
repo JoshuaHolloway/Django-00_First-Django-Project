10869457c15f509d99d6bc1f6db29625d583d93d
joshuaholloway <jhollow6@asu.edu>
2019-06-29 15:59:23 -0500

    added tesla

--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render
 
-def index(request):
+def root(request):
+    return render(request, 'first_app/tesla.html')
+
+def pacman(request):
     return render(request, 'first_app/pacman.html')
