8fae01c2e14082390f9a3dd9f9ca710596159901
joshuaholloway <jhollow6@asu.edu>
2019-06-29 14:49:37 -0500

    added pacman

--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -2,5 +2,4 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    return render(request, 'first_app/index.html')
-
+    return render(request, 'first_app/pacman.html')
